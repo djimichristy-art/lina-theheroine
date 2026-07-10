e68a31870bf6eb0f3f63cdcb350068bfc019fc93
djimichristy-art <djimichristy@gmail.com>
2026-07-10 08:02:29 +0200

    edited the killzone and enemy script, added raycasts in the enemy scene

--- killzone.gd
+++ killzone.gd
@@ -1,13 +1,11 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
-func_on_body_entered(body: Node2D) -> void:
-	if body.name == "player" :
-		print("you died!")
+
+func _on_body_entered(body: Node2D) -> void:
+	print("You die! ")
+	timer.start()
+  
 
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
-
-
-func _on_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
