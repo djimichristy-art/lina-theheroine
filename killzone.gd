625b396f9c95d6a20879d557e81a4f5fb28f60d8
djimichristy-art <djimichristy@gmail.com>
2026-07-10 07:08:13 +0200

    editing the killzone script

--- killzone.gd
+++ killzone.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
-
-func _on_body_entered(body: Node2D) -> void:
-	print("you died!")
-	timer.start()
+func_on_body_entered(body: Node2D) -> void:
+	if body.name == "player" :
+		print("you died!")
 
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
