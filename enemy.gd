7b4445e66360f6e673a4975c33e3c07b6d83e658
djimichristy-art <djimichristy@gmail.com>
2026-07-10 08:12:41 +0200

    edited the enemy script

--- enemy.gd
+++ enemy.gd
@@ -4,6 +4,16 @@ const SPEED = 60
 
 var direction = 1
 
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_castleft: RayCast2D = $RayCastleft
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if ray_cast_right.is_colliding():
+		direction = -1
+	animated_sprite_2d.flip_h = true
+	if ray_castleft.is_colliding():
+		direction = 1
+	animated_sprite_2d.flip_h = false
 	position.x += direction * SPEED * delta
