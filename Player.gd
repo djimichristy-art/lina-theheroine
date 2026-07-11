6b8b8e575775a18af9215a4e98ee766ffc6badd7
djimichristy-art <djimichristy@gmail.com>
2026-07-11 18:12:42 +0200

    edited my player scipt to solve a jump issue

--- Player.gd
+++ Player.gd
@@ -1,33 +1,28 @@
 extends CharacterBody2D
-
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-
 #Get the gravity from the project settings to be synced with RigidBody nodes
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta):
 	#Add the gravity.
-	if not  is_on_floor():
+	if not is_on_floor():
 		velocity.y += gravity * delta
-		
+
 	#  Handle jump.
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	print("jump triggered")
-		
+
 	#Get the input direction: -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
-	
+
 	# Flip the Sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
-	
+
 	# Play animations
 	if is_on_floor():
 		if direction == 0:
