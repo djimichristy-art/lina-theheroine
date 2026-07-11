9ad3ebdc38715734730ab0a52581d4d8d0d738dd
djimichristy-art <djimichristy@gmail.com>
2026-07-11 13:57:40 +0200

    edited the killzone script

--- killzone.gd
+++ killzone.gd
@@ -8,8 +8,11 @@ func _on_body_entered(body: Node2D) -> void:
 		return
 		is_dead = true
 	print("You die! ")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
   
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
