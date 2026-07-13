d26ed3afe5a204314659dee0bbc9445202874c54
djimichristy-art <djimichristy@gmail.com>
2026-07-13 10:34:54 +0200

    edited my killzone script  to fix a dead loop issue

--- killzone.gd
+++ killzone.gd
@@ -3,16 +3,13 @@ extends Area2D
 @onready var timer: Timer = $Timer
 var is_dead = false
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(body):
 	if is_dead:
 		return
 		is_dead = true
-	print("You die! ")
-	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()
+		print("You died!")
 	timer.start()
-  
+
 
 func _on_timer_timeout() -> void:
-	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
