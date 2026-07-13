aab7154eb8099c598a957abf4d02e9bc2d9b4cd7
djimichristy-art <djimichristy@gmail.com>
2026-07-13 18:15:03 +0200

    connected a pressed node to each of the buttons

--- main_menu.gd
+++ main_menu.gd
@@ -1,14 +1,22 @@
-extends Node2D
+extends Control
 
 
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://assets/scenes/levels/level1.tscn")
+ # Called when the node enters the scene tree for the first time
+func _ready():
+	pass # Replace with function body.
 
 
-func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://assets/scenes/options.tscn")
-	
-	
-func _on_quit_pressed():
-	get_tree().quit() 
-	
+# Called every frame. 'delta' is the elapsed time since since the previous 
+func _processed(delta):
+	pass # Replace with function body.
+
+
+func _on_button_pressed() -> void:
+	print("Start pressed")
+
+
+func _on_settings_pressed() -> void:
+	print("Settings pressed")
+
+func _on_exit_pressed() -> void:
+	print("Exit pressed")
