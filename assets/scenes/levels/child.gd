ae0b83e9173cfee58aecd063adc393b22528d9a9
djimichristy-art <djimichristy@gmail.com>
2026-07-14 12:55:24 +0200

    temporarily edited the script to check if it prints something when the player touches the child

--- assets/scenes/levels/child.gd
+++ assets/scenes/levels/child.gd
@@ -2,8 +2,11 @@ extends Area2D
 @export var next_level_path: String = ""
 func _ready() -> void:
 	$AnimatedSprite2D.play("cry")
-	$AudioStreamPlayer2D.play()
+	
 	
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player" and next_level_path != "":
-		get_tree().change_scene_to_file(next_level_path)
+	print("Touched by: ", body.name)
+	if body.name == "Player" :
+		$AudioStreamPlayer2D.play()
+		if next_level_path != "":
+			get_tree().change_scene_to_file(next_level_path)
