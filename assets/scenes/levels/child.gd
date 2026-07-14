d2187f270b6a6d659ad7fd9fd1a4b03a2239c3a6
djimichristy-art <djimichristy@gmail.com>
2026-07-14 18:42:41 +0200

    imported sprites

--- assets/scenes/levels/child.gd
+++ assets/scenes/levels/child.gd
@@ -8,5 +8,7 @@ func _on_body_entered(body: Node2D) -> void:
 	print("Touched by: ", body.name)
 	if body.name == "Player" :
 		$AudioStreamPlayer2D.play()
+		print("Sound played, checking path")
+		print("next_level_path  = ", next_level_path)
 		if next_level_path != "":
 			get_tree().change_scene_to_file(next_level_path)
