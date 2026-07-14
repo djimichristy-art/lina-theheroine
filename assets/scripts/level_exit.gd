4d296a601704926d9719f0b8241a6086a9553fe0
djimichristy-art <djimichristy@gmail.com>
2026-07-14 06:46:31 +0200

    deleted some scripts

--- assets/scripts/level_exit.gd
+++ assets/scripts/level_exit.gd
@@ -1,11 +1,7 @@
 extends Area2D
-
-const FILE_BEGIN = "res://assets/scenes/level2.tscn"
+@export var next_level_path : String = "" 
+	
 
 func _on_body_entered(body: Node2D) -> void:
-	if body. is_in_group("Player"):
-		var current_scene_file = get_tree().level_exit
-		var next_level_number = current_scene_file.to.int() +1
-		
-		var next_level_path = "res://assets/scenes/level2.tscn_" +  str(next_level_number) + ".tscn"
+	if  body.name == "Player" and next_level_path != "":
 		get_tree().change_scene_to_file(next_level_path)
--- levelexit.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.name == "player":
-		game_manager.go_to_next_level()
