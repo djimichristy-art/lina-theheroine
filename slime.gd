4300f181d378a0acd7315637291030cd96c5273f
djimichristy-art <djimichristy@gmail.com>
2026-07-15 16:32:09 +0200

    added a script for the slime

--- slime.gd
+++ slime.gd
@@ -1,11 +1,24 @@
 extends Node2D
 
+const SPEED = 60
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var direction = 1
 
+@onready var ray_cast_right: RayCast2D = $RayCastright
+@onready var ray_cast_left: RayCast2D = $RayCastleft
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if ray_cast_right.is_colliding():
+		direction = -1
+	animated_sprite_2d.flip_h = true
+	if ray_cast_left.is_colliding():
+		direction = 1
+	animated_sprite_2d.flip_h = false
+	position.x += direction * SPEED * delta
+	
+	
+func _on_area_2d_body_entered(body: Node2D) -> void:
+		if body is Player:
+			get_tree().reload_current_scene()
