d40621ff917bf656a5e6b412de1af52896a4c606
djimichristy-art <djimichristy@gmail.com>
2026-07-15 16:57:31 +0200

    Created a new script for the child scene

---
+++ child.gd
@@ -0,0 +1,15 @@
+extends Area2D
+@export var next_level_path: String = ""
+func _ready() -> void:
+	$AnimatedSprite2D.play("cry")
+	
+	
+	
+func _on_body_entered(body: Node2D) -> void:
+	print("Touched by: ", body.name)
+	if body.name == "Player" :
+		$AudioStreamPlayer2D.play()
+		print("Sound played, checking path")
+		print("next_level_path  = ", next_level_path)
+		if next_level_path != "":
+			get_tree().change_scene_to_file(next_level_path)
