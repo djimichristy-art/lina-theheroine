6dd148cc194274358b4a6695169fa1a7d475b5a7
djimichristy-art <djimichristy@gmail.com>
2026-07-15 14:33:37 +0200

    added a key to level two

---
+++ key.gd
@@ -0,0 +1,7 @@
+extends Area2D
+@export var next_level_path : String =""
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player" and next_level_path != "":
+		get_tree().change_scene_to_file(next_level_path)
