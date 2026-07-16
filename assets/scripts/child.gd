b90492a76f43d2c36e52a138e48e7330c0d370d4
djimichristy-art <djimichristy@gmail.com>
2026-07-16 18:48:59 +0200

    saved each scene and script in their respective folders, and added a label to the main menu

--- assets/scripts/child.gd
+++ assets/scripts/child.gd
@@ -1,15 +1,7 @@
 extends Area2D
 @export var next_level_path: String = ""
-func _ready() -> void:
-	$AnimatedSprite2D.play("cry")
-	
-	
+
 	
 func _on_body_entered(body: Node2D) -> void:
-	print("Touched by: ", body.name)
-	if body.name == "Player" :
-		$AudioStreamPlayer2D.play()
-		print("Sound played, checking path")
-		print("next_level_path  = ", next_level_path)
-		if next_level_path != "":
+	if body.name == "Player" and next_level_path != "":
 			get_tree().change_scene_to_file(next_level_path)
--- child.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-@export var next_level_path: String = ""
-
-	
-func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player" and next_level_path != "":
-			get_tree().change_scene_to_file(next_level_path)
