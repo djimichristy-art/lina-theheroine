0c666262495776b2dd142a6943863660a43dfff7
djimichristy-art <djimichristy@gmail.com>
2026-07-16 19:29:14 +0200

    edited the child script to add sound

--- assets/scripts/child.gd
+++ assets/scripts/child.gd
@@ -3,5 +3,5 @@ extends Area2D
 
 	
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player" and next_level_path != "":
+	if body.name == "Player"and next_level_path != "":
 			get_tree().change_scene_to_file(next_level_path)
